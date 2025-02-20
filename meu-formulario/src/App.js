4b1945f3753e0c0e55b53128765c72cc693da2ff
Rangelmello <rangel.melo2018@gmail.com>
2025-02-20 16:44:20 -0300

    adicionado alguns campos no formulario

--- meu-formulario/src/App.js
+++ meu-formulario/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Formulario from './formulario';
 
+
 function App() {
   return (
     <div className="App">
--- meu-formulario/src/formulario.js
+++ meu-formulario/src/formulario.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Formulario() {
   const [formData, setFormData] = useState({
     nome: "",
-    email: "",
-    idade: "",
-    instituicaoDeEnsino:""
+    dataDeNascimento: "",
+    instituicaoDeEnsino: "",
+    anoTurma: "",
+    paisResponsaveis: "",
+    endereco: "",
+    contato: "",
+    dataDeAtendimento: "",
+    presencial:false,
+    contatoTelefonico: false
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, type, checked } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: type === "checkbox" ? checked : value
     });
   };
 
@@ -22,51 +29,121 @@ export default function Formulario() {
   };
 
   return (
-    <div className="App-link">
-      <h2 className="">Formulário</h2>
+    <div className="container">
+      <h2 className="formulario">Formulário</h2>
+      <h4 className="">FICHA DE ACOMPANHAMENTO DO NÚCLEO DE MEDIAÇÃO SOCIOESCOLAR - NUMESE</h4>
       <form onSubmit={handleSubmit}>
-        <div className="">
-          <label className="">Nome</label>
-          <input
-            type="text"
-            name="nome"
-            value={formData.nome}
-            onChange={handleChange}
-            className=""
-          />
+        <div className="row">
+          <div className="col-md-3">
+            <label>Nome</label>
+            <input
+              type="text"
+              name="nome"
+              value={formData.nome}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Data de Nascimento</label>
+            <input
+              type="date"
+              name="dataDeNascimento"
+              value={formData.dataDeNascimento}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Instituição de Ensino</label>
+            <input
+              type="text"
+              name="instituicaoDeEnsino"
+              value={formData.instituicaoDeEnsino}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Ano/Turma</label>
+            <input
+              type="text"
+              name="anoTurma"
+              value={formData.anoTurma}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Pais/responsável</label>
+            <input
+              type="text"
+              name="paisResponsaveis"
+              value={formData.paisResponsaveis}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Endereço</label>
+            <input
+              type="text"
+              name="endereco"
+              value={formData.endereco}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Contato telefônico</label>
+            <input
+              type="tel"
+              name="contato"
+              value={formData.contato}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <label>Data de Atendimento</label>
+            <input
+              type="date"
+              name="dataDeAtendimento"
+              value={formData.dataDeAtendimento}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+          <h7 className="">MODALIDADE DE ATENDIMENTO</h7>
+          <div className="col-md-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="presencial"
+              checked={formData.aceitarTermos}
+              onChange={handleChange}
+              id="presencial"
+            />
+            <label className="form-check-label" htmlFor="presencial">
+              Presencial
+            </label>
+          </div>
+          <div className="col-md-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="contatoTelefonico"
+              checked={formData.aceitarTermos}
+              onChange={handleChange}
+              id="contatoTelefonico"
+            />
+            <label className="form-check-label" htmlFor="contatoTelefonico">
+              Contato Telefônico
+            </label>
+          </div>
         </div>
-        <div className="">
-          <label className="">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className=""
-          />
-        </div>
-        <div className="">
-          <label className="">Idade</label>
-          <input
-            type="number"
-            name="idade"
-            value={formData.idade}
-            onChange={handleChange}
-            className=""
-          />
-        </div>
-        <div className="">
-          <label className="">Instituição de Ensino</label>
-          <input
-            type="text"
-            name="instituicaoDeEnsino"
-            value={formData.instituicaoDeEnsino}
-            onChange={handleChange}
-            className=""
-          />
-        </div>
-        <button type="submit" className="">
-          Enviar
+        <button type="submit" className="btn btn-primary mt-3">
+          Salvar
         </button>
       </form>
     </div>
